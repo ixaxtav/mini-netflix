95cff537197dd6d1e221d31791a4b704754ff5ef
Ixax Tavarez <ixaxtav@gmail.com>
2024-01-07 19:36:18 -0500

    create home page with 5 movie post

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,43 @@
+import Link from "next/link";
 import Image from "next/image";
 import styles from "./page.module.scss";
 
-export default function Home() {
-  return <main></main>;
+interface Movie {
+  Title: string;
+  Year: string;
+  Poster: string;
+  Genre: string;
+  imdbID: string;
+}
+
+async function getWarMovies(): Promise<Movie[]> {
+  const res = await fetch(`https://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=war&type=movie`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const { Search } = await res.json();
+  return Search.slice(0, 5);
+}
+
+export default async function Home() {
+  const data = await getWarMovies();
+
+  return (
+    <main className={styles.container} role="main">
+      <h1>Only On Mini Netflix</h1>
+      <section className={styles.cardRow}>
+        {data.map((movie) => (
+          <article key={movie.imdbID} className={styles.card}>
+            <Link href={`/movie/details/${movie.imdbID}`}>
+              <div className={styles.cardLink}>
+                <Image src={movie.Poster} alt={`Poster of the movie ${movie.Title}`} width={300} height={450} />
+              </div>
+            </Link>
+          </article>
+        ))}
+      </section>
+    </main>
+  );
 }
