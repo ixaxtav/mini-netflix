af79e9e07691eff3ebf8a49557959ecf3d105d8a
Ixax Tavarez <ixaxtav@gmail.com>
2024-01-07 21:13:58 -0500

    revert deployment code changes

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   images: {
     domains: ["m.media-amazon.com"],
   },
-  basePath: "/mini-netflix",
-  assetPrefix: "/mini-netflix/",
-  output: "export",
 };
 
 module.exports = nextConfig;
